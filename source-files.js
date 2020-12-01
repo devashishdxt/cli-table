8e936a9c99a9b26489e264fad01a72ff73462151
devashishdxt <devashishdxt@users.noreply.github.com>
2020-12-01 20:35:21 +0000

    Automated deployment: Tue Dec  1 20:35:21 UTC 2020 9793cd2eabbe5c23cbb0739a8edfd4d68f53fb1e

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["cli_table"] = {"name":"","dirs":[{"name":"format","files":["cell.rs","table.rs"]}],"files":["cell.rs","error.rs","format.rs","lib.rs","row.rs","table.rs"]};
+sourcesIndex["cli_table"] = {"name":"","dirs":[{"name":"format","files":["cell.rs","table.rs"]}],"files":["cell.rs","dimension.rs","error.rs","format.rs","lib.rs","row.rs","table.rs"]};
 createSourceSidebar();

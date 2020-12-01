c405b2eb964cb453f59e340aa75ca43ce7713b0b
devashishdxt <devashishdxt@users.noreply.github.com>
2020-12-01 11:14:35 +0000

    Automated deployment: Tue Dec  1 11:14:35 UTC 2020 d4f717ec75ea80317658c260f731547f217d462a

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["cli_table"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"cli_table/enum.Error.html\" title=\"enum cli_table::Error\">Error</a>","synthetic":false,"types":["cli_table::error::Error"]}];
+implementors["cli_table"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

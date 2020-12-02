c0a588bcd1918b67e78f935e85b4b3be559fdae3
devashishdxt <devashishdxt@users.noreply.github.com>
2020-12-02 21:33:11 +0000

    Automated deployment: Wed Dec  2 21:33:11 UTC 2020 a1dd5c6dffa887bca3885eed09a18fe9032b120f

--- implementors/core/fmt/trait.Display.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["cli_table"] = [{"text":"impl Display for Error","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

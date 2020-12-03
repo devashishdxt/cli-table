ff07af641bede69f8a63560c966d0cd8502d32aa
devashishdxt <devashishdxt@users.noreply.github.com>
2020-12-03 06:43:57 +0000

    Automated deployment: Thu Dec  3 06:43:57 UTC 2020 106a9807acf45c9b3d3945dd6c644e5e148c2014

--- implementors/core/convert/trait.From.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["cli_table"] = [{"text":"impl&lt;T:&nbsp;Display&gt; From&lt;T&gt; for CellStruct","synthetic":false,"types":[]},{"text":"impl&lt;C:&nbsp;Cell, T:&nbsp;IntoIterator&lt;Item = C&gt;&gt; From&lt;T&gt; for RowStruct","synthetic":false,"types":[]},{"text":"impl&lt;C:&nbsp;Row, T:&nbsp;IntoIterator&lt;Item = C&gt;&gt; From&lt;T&gt; for TableStruct","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["cli_table"] = {"name":"","files":["buffers.rs","cell.rs","format.rs","lib.rs","row.rs","style.rs","table.rs","utils.rs"]};
+sourcesIndex["cli_table"] = {"name":"","files":["buffers.rs","cell.rs","format.rs","lib.rs","row.rs","style.rs","table.rs","title.rs","utils.rs"]};
 createSourceSidebar();

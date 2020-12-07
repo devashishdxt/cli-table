bc23608bd3bc182f9fcfbcda538208bfc1943bee
devashishdxt <devashishdxt@users.noreply.github.com>
2020-12-07 16:02:24 +0000

    deploy: ccd7aa72196ba6d9815b5322f09ffa072667560a

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["cli_table"] = {"name":"","files":["buffers.rs","cell.rs","format.rs","lib.rs","row.rs","style.rs","table.rs","title.rs","utils.rs"]};
+sourcesIndex["cli_table_derive"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();

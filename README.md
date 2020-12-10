69b97c07f34f7308f5c6dd3d56ebeeeabad929f0
Devashish Dixit <devashishdxt@gmail.com>
2020-12-10 16:50:32 +0800

    Update documentation and example with separate title

--- README.md
+++ README.md
@@ -22,12 +22,15 @@ cli-table = "0.4"
 use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
 
 let table = vec![
-    vec!["Name".cell().bold(true), "Age (in years)".cell().bold(true)],
     vec!["Tom".cell(), 10.cell().justify(Justify::Right)],
     vec!["Jerry".cell(), 15.cell().justify(Justify::Right)],
     vec!["Scooby Doo".cell(), 20.cell().justify(Justify::Right)],
 ]
 .table()
+.title(vec![
+    "Name".cell().bold(true),
+    "Age (in years)".cell().bold(true),
+])
 .bold(true);
 
 assert!(print_stdout(table).is_ok());
--- cli-table/examples/simple.rs
+++ cli-table/examples/simple.rs
@@ -4,12 +4,15 @@ use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
 
 fn main() -> Result<()> {
     let table = vec![
-        vec!["Name".cell().bold(true), "Age (in years)".cell().bold(true)],
         vec!["Tom".cell(), 10.cell().justify(Justify::Right)],
         vec!["Jerry".cell(), 15.cell().justify(Justify::Right)],
         vec!["Scooby Doo".cell(), 20.cell().justify(Justify::Right)],
     ]
     .table()
+    .title(vec![
+        "Name".cell().bold(true),
+        "Age (in years)".cell().bold(true),
+    ])
     .bold(true);
 
     print_stdout(table)

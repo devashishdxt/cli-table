5f6b9b121746ed1a62c2ee0583a52425a723a00d
Devashish Dixit <devashishdxt@gmail.com>
2021-03-16 07:34:10 +0000

    Change user creation script

--- .devcontainer/scripts/create-user.sh
+++ .devcontainer/scripts/create-user.sh
@@ -3,6 +3,10 @@ USERNAME=${1}
 USER_UID=${2}
 USER_GID=${3}
 
+if [ ! "$USERNAME" ];then
+   USERNAME=vscode
+fi
+
 # Create the user
 addgroup --gid $USER_GID $USERNAME
 adduser --uid $USER_UID --gid $USER_GID --disabled-password $USERNAME

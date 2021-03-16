10e94789e93e8006f706a8d5a108a5dc59e1488a
Devashish Dixit <devashishdxt@gmail.com>
2021-03-16 07:46:49 +0000

    Use root user when username is not present

--- .devcontainer/scripts/create-user.sh
+++ .devcontainer/scripts/create-user.sh
@@ -3,10 +3,8 @@ USERNAME=${1}
 USER_UID=${2}
 USER_GID=${3}
 
-if [ $USERNAME == "" ]; then
-   exit 0
+if [ ! -z "$USERNAME" ]; then
+   # Create the user
+   addgroup --gid $USER_GID $USERNAME
+   adduser --uid $USER_UID --gid $USER_GID --disabled-password $USERNAME
 fi
-
-# Create the user
-addgroup --gid $USER_GID $USERNAME
-adduser --uid $USER_UID --gid $USER_GID --disabled-password $USERNAME

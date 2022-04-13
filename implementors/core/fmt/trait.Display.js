1480f6356f8ba0aa52a4277abeec49b4d2794400
devashishdxt <devashishdxt@users.noreply.github.com>
2022-04-13 01:32:01 +0000

    deploy: 0b2b3f197997d2a8fab4b8b4cfbf977262c7a5f0

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["cli_table"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"cli_table/struct.TableDisplay.html\" title=\"struct cli_table::TableDisplay\">TableDisplay</a>","synthetic":false,"types":["cli_table::display::TableDisplay"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

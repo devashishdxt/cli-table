717b73b50ce0ff92437b7947240fb54a023b5585
devashishdxt <devashishdxt@users.noreply.github.com>
2022-05-25 01:56:32 +0000

    deploy: b41e3ba18ee5a9a56b6dd2ad28b255b6fbdfe1ea

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["cli_table"] = {"name":"","files":["buffers.rs","cell.rs","csv.rs","display.rs","format.rs","lib.rs","row.rs","style.rs","table.rs","title.rs","utils.rs"]};
 sourcesIndex["cli_table_derive"] = {"name":"","dirs":[{"name":"context","files":["container.rs","fields.rs"]}],"files":["context.rs","lib.rs","table.rs","utils.rs"]};
 createSourceSidebar();
